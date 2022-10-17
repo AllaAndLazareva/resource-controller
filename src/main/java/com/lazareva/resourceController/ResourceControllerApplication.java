bebfc952923aac33fea286d394a7abaecdd8211a
AllaAndLazareva <alla.soikina@yandex.ru>
2022-10-17 16:10:34 +0300

    Initial commit

---
+++ src/main/java/com/lazareva/resourceController/ResourceControllerApplication.java
@@ -0,0 +1,13 @@
+package com.lazareva.resourceController;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourceControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResourceControllerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lazareva/resourceController/ResourceControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lazareva.resourceController;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourceControllerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
