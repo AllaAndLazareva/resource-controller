578c563ccf6b58efd3e78310b6fb59a875e728a3
vitaliy.soykin <vitaliy.soykin@vizor-games.com>
2022-10-17 20:41:09 +0300

    revert test configurations

--- src/test/java/com/lazareva/resourceController/ResourceControllerApplicationTests.java
+++ src/test/java/com/lazareva/resourceController/ResourceControllerApplicationTests.java
@@ -3,10 +3,11 @@ package com.lazareva.resourceController;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-
+@SpringBootTest
 class ResourceControllerApplicationTests {
 
 
+	@Test
 	void contextLoads() {
 	}
 
