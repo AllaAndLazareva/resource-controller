a32b9975682c9a1a5d71ef682c07a72e1df5db4c
vitaliy.soykin <vitaliy.soykin@vizor-games.com>
2022-10-17 19:21:35 +0300

    test

--- src/test/java/com/lazareva/resourceController/ResourceControllerApplicationTests.java
+++ src/test/java/com/lazareva/resourceController/ResourceControllerApplicationTests.java
@@ -3,10 +3,10 @@ package com.lazareva.resourceController;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class ResourceControllerApplicationTests {
 
-	@Test
+
 	void contextLoads() {
 	}
 
