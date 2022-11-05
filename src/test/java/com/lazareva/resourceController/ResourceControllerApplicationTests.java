575a65bcf55bf938e51d142284e8fb7f96aff7a6
vitaliysoykin <ipvital121911@gmail.com>
2022-11-05 17:11:31 +0300

    my config for postgress

--- src/test/java/com/lazareva/resourceController/ResourceControllerApplicationTests.java
+++ src/test/java/com/lazareva/resourceController/ResourceControllerApplicationTests.java
@@ -3,11 +3,11 @@ package com.lazareva.resourceController;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+/*@SpringBootTest*/
 class ResourceControllerApplicationTests {
 
 
-	@Test
+	/*@Test*/
 	void contextLoads() {
 	}
 
